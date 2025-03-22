45614a469594a1bdd0a24d71fb854fd796146de4
Chirag Parmar <97354775+webdev-chirag@users.noreply.github.com>
2025-03-22 21:02:54 +0530

    testing commit

---
+++ app/home/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import ContactSection from "@/components/Contact";
+import ExperienceEducationSection from "@/components/ExperienceEducation";
+import HeroSection from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import ProjectsSection from "@/components/Projects";
+import SkillsSection from "@/components/Skills";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <SkillsSection />
+      <ExperienceEducationSection />
+      <ProjectsSection />
+      <ContactSection />
+    </>
+  );
+}

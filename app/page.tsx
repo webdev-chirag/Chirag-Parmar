37134d2dd164397f5466375145e4b5baf03d0b3d
Chirag Parmar <97354775+webdev-chirag@users.noreply.github.com>
2025-05-04 19:29:24 +0530

    toast message position change

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,19 @@ import { Toaster } from "react-hot-toast";
 export default function Home() {
   return (
     <>
-      <Toaster position="top-right" />
       <Navbar />
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          duration: 3000,
+          style: { zIndex: 9999 },
+        }}
+        reverseOrder={false}
+        containerStyle={{
+          marginBottom: "60px",
+        }}
+      />
+
       <HeroSection />
       <SkillsSection />
       <ExperienceEducationSection />
